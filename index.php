d1ac9a46b13f0b3aa38436740a5e39e0011f6bc4
Night 狼 <dhenys.1002@gmail.com>
2020-12-26 01:26:09 +0700

    Add Script
    Add Script view all data and view 1 data

--- index.php
+++ index.php
@@ -18,3 +18,41 @@ mysql_select_db($database) or die ("<h1><center>Database tidak terhubung dengan 
 <p>Apakah Database sudah dibuat ?</p>" . mysql_error());
 
 # Script Switch case untuk Rest API
+
+# untuk membuat pilihan contoh [URL]?fiture=.....
+
+@$fiture = $_GET['fiture'];
+
+# Bagian Admin
+
+switch ($fiture) {
+	
+	# Untuk menampilkan semua data "admin" tanpa kecuali
+	# Contoh Link [URL]?fiture=viewalladm
+	
+	case "viewalladm":
+		$query_admin = mysql_query("SELECT * FROM admin") or die(mysql_error());
+		$data_array = array();
+		while ($data = mysql_fetch_assoc($query_admin)) {
+			$data_array[] = $data;
+			}
+			echo json_encode($data_array);
+			break;
+		  
+		  # Untuk view data "admin" dalam 1 id     
+		  # Contoh Link [URL]?fiture=viewadm&id=[nomor id pada field admin]
+		  
+	case "viewadm":
+		@$id = $_GET['id'];
+		$query_admin = mysql_query("SELECT * FROM admin WHERE id='$id'") or die(mysql_error());
+		$data_array = array();
+		$data_array = mysql_fetch_assoc($query_admin);
+		echo "[" . json_encode($data_array) . "]";
+		break;
+				
+	default:
+	echo "<center><h1>Oh No...</h1></center>"."<p>Link Not Found!</p>";
+	break;
+}
+
+?>
