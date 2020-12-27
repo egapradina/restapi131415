a567c83369302573266bb990101a7bdd8cbe2d8a
Night 狼 <dhenys.1002@gmail.com>
2020-12-27 21:27:32 +0700

    Finish Ver1.10
    Update Anggota, Buku

--- index.php
+++ index.php
@@ -23,7 +23,11 @@ mysql_select_db($database) or die ("<h1><center>Database tidak terhubung dengan 
 
 @$fiture = $_GET['fiture'];
 
-# Bagian Admin
+####################################
+###                              ###
+###        Bagian Admin          ###
+###                              ###
+####################################
 
 switch ($fiture) {
 	
@@ -39,8 +43,8 @@ switch ($fiture) {
 			echo json_encode($data_array);
 			break;
 		  
-		  # Untuk view data "admin" dalam 1 id     
-		  # Contoh Link [URL]?fiture=viewadm&id=[nomor id pada field admin]
+	# Untuk view data "admin" dalam 1 id     
+	# Contoh Link [URL]?fiture=viewadm&id=[nomor id pada field admin]
 		  
 	case "viewadm":
 		@$id = $_GET['id'];
@@ -94,10 +98,170 @@ switch ($fiture) {
 				}
 				break;
 				
-# Bagian Anggota
+####################################
+###                              ###
+###        Bagian Anggota        ###
+###                              ###
+####################################
+
+	# Untuk menampilkan semua data "anggota" tanpa kecuali
+	# Contoh Link [URL]?fiture=viewallang
+	
+	case "viewallang":
+		$query_anggota = mysql_query("SELECT * FROM anggota") or die(mysql_error());
+		$data_array = array();
+		while ($data = mysql_fetch_assoc($query_anggota)) {
+			$data_array[] = $data;
+			}
+			echo json_encode($data_array);
+			break;
 				
+	# Untuk view data "anggota" dalam 1 id     
+	# Contoh Link [URL]?fiture=viewang&id=[nomor id pada field anggota]
+		  
+	case "viewang":
+		@$id = $_GET['id'];
+		$query_anggota = mysql_query("SELECT * FROM anggota WHERE id='$id'") or die(mysql_error());
+		$data_array = array();
+		$data_array = mysql_fetch_assoc($query_anggota);
+		echo "[" . json_encode($data_array) . "]";
+		break;
+		
+	# Untuk menambah Data pada "anggota" 
+    # Contoh Link [URL]?fiture=insang&nm_agt=[Isi Nama Angota]&pwd_agt=[Isi Password Anggota]&status=[Status diisi 1 / 0]&tgl=[Tanggal YYYY-MM-DD]
+	# ! Warning ! Status 1 artinya aktif dan 0 artinya tidak aktif
+        
+    case "insang":
+		@$nm_agt = $_GET['nm_agt'];
+		@$pwd_agt = $_GET['pwd_agt'];
+		@$status = $_GET['status'];
+		@$tgl = $_GET['tgl'];
+		$query_insert_anggota = mysql_query("INSERT INTO anggota (nm_agt, pwd_agt, status, tgl) VALUES('$nm_agt', '$pwd_agt', '$status', '$tgl')");
+		if ($query_insert_anggota) {
+			echo "<h1>Data Anggota Berhasil Disimpan ! </h1>";
+			} else {
+				echo "Error Inser" . mysql_error();
+				}
+				break;
+				
+	# Untuk mengubah / edit data "anggota"
+	# Contoh Link [URL]?fiture=editang&nm_agt=[Nama Anggota]&pwd_agt=[Password Anggota]&status=[Status diisi 1 / 0]&tgl[Tanggal YYYY-MM-DD]&id=[id Anggota yang akan diubah]
+	# ! Warning ! Status 1 artinya aktif dan 0 artinya tidak aktif
+        
+    case "editang":
+        @$nm_agt = $_GET['nm_agt'];
+		@$pwd_agt = $_GET['pwd_agt'];
+		@$status = $_GET['status'];
+		@$tgl = $_GET['tgl'];
+        @$id = $_GET['id'];
+        $query_update_anggota= mysql_query("UPDATE anggota SET nm_agt='$nm_agt', pwd_agt='$pwd_agt', status='$status', tgl='$tgl' WHERE id='$id'");
+        if ($query_update_anggota) {
+            echo "<h1>Data Anggota Berhasil Diubah ! </h1>";
+        } else {
+            echo mysql_error();
+        }
+        break;
+		
+	# Untuk menghapus Data pada "anggota" 
+    # Contoh Link [URL]?fiture=dltang&id=[Nomor id]
+	 
+    case "dltang":
+		@$id = $_GET['id'];
+		$query_delete_anggota = mysql_query("DELETE FROM anggota WHERE id='$id'");
+		if ($query_delete_anggota) {
+			echo "<h1>Data Anggota Berhasil Dihapus!</h1>";
+			} else {
+				echo mysql_error();
+				}
+				break;
+				
+####################################
+###                              ###
+###         Bagian Buku          ###
+###                              ###
+####################################
+
+	# Untuk menampilkan semua data "buku" tanpa kecuali
+	# Contoh Link [URL]?fiture=viewallbk
+	
+	case "viewallbk":
+		$query_buku = mysql_query("SELECT * FROM buku") or die(mysql_error());
+		$data_array = array();
+		while ($data = mysql_fetch_assoc($query_buku)) {
+			$data_array[] = $data;
+			}
+			echo json_encode($data_array);
+			break;
+				
+	# Untuk view data "buku" dalam 1 id     
+	# Contoh Link [URL]?fiture=viewbk&id=[nomor id pada field buku]
+		  
+	case "viewbk":
+		@$id = $_GET['id'];
+		$query_buku = mysql_query("SELECT * FROM buku WHERE id='$id'") or die(mysql_error());
+		$data_array = array();
+		$data_array = mysql_fetch_assoc($query_buku);
+		echo "[" . json_encode($data_array) . "]";
+		break;
+		
+	# Untuk menambah Data pada "buku" 
+    # Contoh Link [URL]?fiture=insbk&judul=[Isi Judul Buku]&kategori=[Isi Kategori Buku]&status=[Status diisi 1 / 0]&stok=[Isi Stok Buku]&isbn=[Isi Nomor ISBN Buku]
+	# ! Warning ! Status 1 artinya aktif dan 0 artinya tidak aktif
+        
+    case "insbk":
+		@$judul = $_GET['judul'];
+		@$kategori = $_GET['kategori'];
+		@$status = $_GET['status'];
+		@$stok  = $_GET['stok'];
+		@$isbn  = $_GET['isbn'];
+		$query_insert_anggota = mysql_query("INSERT INTO buku (judul, kategori, status, stok, isbn) VALUES('$judul', '$kategori', '$status', '$stok', '$isbn')");
+		if ($query_insert_anggota) {
+			echo "<h1>Data Buku Berhasil Disimpan ! </h1>";
+			} else {
+				echo "Error Inser" . mysql_error();
+				}
+				break;
+				
+	# Untuk mengubah / edit data "buku"
+	# Contoh Link [URL]?fiture=editbk&judul=[Isi Judul Buku]&kategori=[Isi Kategori Buku]&status=[Status diisi 1 / 0]&stok=[Isi Stok Buku]&isbn=[Isi Nomor ISBN Buku]&id=[id Buku yang akan diubah]
+	# ! Warning ! Status 1 artinya aktif dan 0 artinya tidak aktif
+        
+    case "editbk":
+        @$judul = $_GET['judul'];
+		@$kategori = $_GET['kategori'];
+		@$status = $_GET['status'];
+		@$stok  = $_GET['stok'];
+		@$isbn  = $_GET['isbn'];
+		@$id = $_GET['id'];
+        $query_update_buku= mysql_query("UPDATE buku SET judul='$judul', kategori='$kategori', status='$status', stok='$stok', isbn='$isbn' WHERE id='$id'");
+        if ($query_update_buku) {
+            echo "<h1>Data Buku Berhasil Diubah ! </h1>";
+        } else {
+            echo mysql_error();
+        }
+        break;
+		
+	# Untuk menghapus Data pada "buku" 
+    # Contoh Link [URL]?fiture=dltbk&id=[Nomor id]
+	 
+    case "dltbk":
+		@$id = $_GET['id'];
+		$query_delete_buku= mysql_query("DELETE FROM buku WHERE id='$id'");
+		if ($query_delete_buku) {
+			echo "<h1>Data Buku Berhasil Dihapus!</h1>";
+			} else {
+				echo mysql_error();
+				}
+				break;
+				
+####################################
+###                              ###
+###              END             ###
+###                              ###
+####################################				
 	default:
-	echo "<center><h1>Oh No...</h1></center>"."<p>Link Not Found!</p>";
+	echo "<center><h1>Oh No...</h1></center>"."<p>Link Not Found!</p>"."<ul>
+	<il><i>* Please Check Link or URL !</i></il></ul>";
 	break;
 }
 
